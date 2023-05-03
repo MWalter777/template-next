7e28c2f2c25e33e00c7875591fefa80b4129e1e6
Walter Marroquin <waltermarrom777@gmail.com>
2023-05-03 12:42:06 -0600

    file upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,10 @@ import 'swiper/css/effect-flip';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import 'react-dual-listbox/lib/react-dual-listbox.css';
 
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
--- src/pages/forms/editors/index.jsx
+++ src/pages/forms/editors/index.jsx
@@ -10,12 +10,11 @@ import {
 } from 'reactstrap';
 import UiContent from '../../../Components/Common/UiContent';
 
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import BreadCrumb from '../../../Components/Common/BreadCrumb';
 
 import { useQuill } from 'react-quilljs';
 import Layout from '@/Layouts';
+import dynamic from 'next/dynamic';
 
 const FormEditor = () => {
 	const { quillRef } = useQuill();
@@ -24,30 +23,6 @@ const FormEditor = () => {
 			<UiContent />
 			<Container fluid>
 				<BreadCrumb title='Editors' pageTitle='Forms' />
-				<Row>
-					<Col lg={12}>
-						<Card>
-							<CardHeader className='align-items-center d-flex'>
-								<h4 className='card-title mb-0'>Ckeditor Classic Editor</h4>
-							</CardHeader>
-							<CardBody>
-								<Form method='post'>
-									<CKEditor
-										editor={ClassicEditor}
-										data='<p>Hello from CKEditor 5!</p>'
-										onReady={(editor) => {
-											// You can store the "editor" and use when it is needed.
-										}}
-										// onChange={(editor) => {
-										//    editor.getData();
-										// }}
-									/>
-								</Form>
-							</CardBody>
-						</Card>
-					</Col>
-				</Row>
-
 				<Row className='mt-2'>
 					<Col lg={12}>
 						<div className='justify-content-between d-flex align-items-center mb-3'>
@@ -72,4 +47,4 @@ const FormEditor = () => {
 	);
 };
 
-export default FormEditor;
+export default dynamic(() => Promise.resolve(FormEditor), { ssr: false });
